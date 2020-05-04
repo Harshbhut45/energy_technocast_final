c4a9c7fc1d46dbcbc9660ebd92214b4444a71e19
Jhalak Javiya <jhalakjaviya@gmail.com>
2020-05-04 17:27:19 +0530

    maps changes

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -47,8 +47,7 @@
                             <h3>Locate Us</h3>
                             <div class="tagcloud">
                                 <div style="min-height:100px; width:100%;">
-                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7389.911935219279!2d70.78029115242875!3d22.16574808577109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3959ca77aaaaaaab%3A0x25fefb3f22ba150e!2sEnergy+Techno+Cast+Pvt+Ltd!5e0!3m2!1sen!2sin!4v1545638892496" width="100%" height="200" frameborder="0" style="border:0" allowfullscreen=""></iframe>
-
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3699.175373046045!2d70.79452151494948!3d22.004590385477908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3958381400000001%3A0x91f171b2ee76f77d!2sEnergy%20Technocast%20Pvt%20Ltd!5e0!3m2!1sen!2sin!4v1588593273186!5m2!1sen!2sin"  width="100%" height="200" frameborder="0" style="border:0" allowfullscreen=""></iframe>
                                 </div>
                             </div>
                         </div>
